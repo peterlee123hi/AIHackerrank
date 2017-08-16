536cb2b83847a8adbcad3981f44119b3750c58d6
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-08-15 17:01:50 -0700

    Finished searches

---
+++ Search/AStar/PacmanAStar.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for(int i=a;i<n;i++)
+typedef pair<int,int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+
+int pacR, pacC;
+int foodR, foodC;
+int n, m;
+
+int dr[] = {-1, 0, 0, 1};
+int dc[] = {0, -1, 1, 0};
+
+char** grid;
+
+vii path;
+
+bool inbounds(ii pos) {
+    return 0 <= pos.first && pos.first < n && 0 <= pos.second && pos.second < m;
+}
+
+int manhattanDistance(ii first, ii second) {
+    return abs(first.first - second.first) + abs(first.second - second.second);
+}
+
+int main() {
+    cin >> pacR >> pacC >> foodR >> foodC >> n >> m;
+    grid = new char*[n];
+    rep(i,0,n) {
+        grid[i] = new char[m];
+        rep(j,0,m) cin >> grid[i][j];
+    }
+
+    /* Begin A* */
+
+    priority_queue< iii, viii, greater<iii> > fringe;
+    fringe.push(iii(manhattanDistance(ii(pacR, pacC), ii(foodR, foodC)), ii(pacR, pacC)));
+    
+    int dist[n][m];
+    memset(dist, -1, sizeof dist);
+    dist[pacR][pacC] = 0;
+
+    int pred[n][m];
+    pred[pacR][pacC] = -1;
+
+    while (!fringe.empty()) {
+        iii state = fringe.top(); fringe.pop();
+        ii pos = state.second;
+        if (state.first > dist[pos.first][pos.second] + manhattanDistance(pos, ii(foodR, foodC))) continue;
+        if (pos.first == foodR && pos.second == foodC) {
+            break;
+        }
+        rep(d,0,4) {
+            ii next = ii(pos.first + dr[d], pos.second + dc[d]);
+            int priority = dist[pos.first][pos.second] + 1 + manhattanDistance(next, ii(foodR, foodC));
+
+            // Only compare the distance from the start...
+            if (inbounds(next) && 
+                (dist[next.first][next.second] > dist[pos.first][pos.second] + 1 || dist[next.first][next.second] == -1) &&
+                grid[next.first][next.second] != '%') {
+                pred[next.first][next.second] = d;
+                dist[next.first][next.second] = dist[pos.first][pos.second] + 1;
+                fringe.push(iii(priority, next));
+            }
+        }
+    }
+
+    ii current = ii(foodR, foodC);
+    while (pred[current.first][current.second] != -1) {
+        path.push_back(current);
+        int d = pred[current.first][current.second];
+        ii next = ii(current.first - dr[d], current.second - dc[d]);
+        current = next;
+    }
+    path.push_back(current);
+    reverse(path.begin(), path.end());
+
+    /* End DFS */
+
+    cout << ((int) path.size()) - 1 << endl;
+    rep(i,0,((int) path.size())) {
+        ii pos = path[i];
+        cout << pos.first << " " << pos.second << endl;
+    }
+
+    /*
+    rep(i,0,((int) path.size())) {
+        ii pos = path[i];
+        if (pos.first == pacR && pos.second == pacC) continue;
+        if (pos.first == foodR && pos.second == foodC) continue;
+        grid[pos.first][pos.second] = '*';
+    }
+    rep(i,0,n) {
+        rep(j,0,m) {
+            cout << grid[i][j];
+        }
+        cout << endl;
+    }
+    */
+
+    return 0;
+}
